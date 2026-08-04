ffdae066cad9f31183e5b0f0eb568bb75433e5d8
Ege <100532870+egehasanz@users.noreply.github.com>
2026-08-04 11:34:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOTTOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True
+
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} olarak giriş yapıldı!")
+    await bot.load_extension("cogs.moderation")
+    await bot.load_extension("cogs.tickets")
+    try:
+        synced = await bot.tree.sync()
+        print(f"{len(synced)} adet slash komutu senkronize edildi.")
+    except Exception as e:
+        print(e)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Bu komutu kullanmak için gerekli yetkiye sahip değilsin.", delete_after=5)
+    elif isinstance(error, commands.CommandNotFound):
+        return
+    else:
+        print(f"Bir hata oluştu: {error}")
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
